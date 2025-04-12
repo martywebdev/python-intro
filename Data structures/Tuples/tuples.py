dd2ba864301642ba39005bcc1b8d8ae3b68ec7d9
martyumlas <martyumlas@gmail.com>
2025-04-12 10:49:29 +0800

    initial commit upto nested list

---
+++ Data structures/Tuples/tuples.py
@@ -0,0 +1,15 @@
+alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
+# print(???)
+
+
+# my_tuple = ???
+
+# print(type(my_tuple))
+
+test = 'test', 'exam', 'inuman'
+
+print(test[1])
+
+
---
+++ Strings/Concatenation/concatenation.py
@@ -0,0 +1,5 @@
+hello = "Hello"
+world = 'World'
+
+hello_world = f"{hello} {world}"
+print(hello_world)      # Note: you should print "Hello World"

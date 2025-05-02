240b15c3792a615d03840d21ca97e499fbc7f8a8
martyumlas <martyumlas@gmail.com>
2025-05-02 22:30:23 +0800

    return value

--- Functions/Return value/return_keyword.py
+++ Functions/Return value/return_keyword.py
@@ -10,13 +10,16 @@ def fib(n):
     Return a list containing the Fibonacci series up to n."""
     result = []
     a = 1
-    b = # Assign an initial value to the b variable
+    b = 1
     while a < n:
         result.append(a)
         tmp_var = b
         # Update the b variable with a sum
+        b = a + b
         # Restore the old value of the b variable to the a variable from the temp
+        a = tmp_var
     # Here we need to return the result to the caller
+    return result
 
 
 if __name__ == '__main__':

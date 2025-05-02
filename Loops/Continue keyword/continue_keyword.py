80b646da9cb5e731132dbc6cc51af067fcedd63d
martyumlas <martyumlas@gmail.com>
2025-05-02 22:10:25 +0800

    start of functions

--- Loops/Continue keyword/continue_keyword.py
+++ Loops/Continue keyword/continue_keyword.py
@@ -4,6 +4,7 @@ for i in range(5):
     print(i)
 
 for x in range(10):
-    # Add a condition
+    if x % 2 != 1:
+        continue
         # Add a keyword to skip print(x) for this loop
     print(x)
--- Loops/Else with loops 2/else_with_loops2.py
+++ Loops/Else with loops 2/else_with_loops2.py
@@ -1,6 +1,12 @@
 def contains_even_number(lst):
-    pass
-
+    for i in lst:
+        if i % 2 == 0:
+            msg = f'List {lst} contains an even number.'
+            print(msg)
+            break
+    else:
+        msg = f'List {lst} does not contain an even number.'
+        print(msg)
 
 contains_even_number([1, 9, 8])
 contains_even_number([1, 3, 5])
